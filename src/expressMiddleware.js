fc6d6fa296669bc85ded982fa45a5b7a5dc34d5b
birbalkumarkumar <birbal.kumar@kore.com>
2016-12-20 18:11:26 +0530

    Update expressMiddleware.js

--- src/expressMiddleware.js
+++ src/expressMiddleware.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var _require = require('zipkin');
-
+var Promise = require('bluebird'); // it's not been used in this 
 var Annotation = _require.Annotation;
 var Header = _require.HttpHeaders;
 var _require$option = _require.option;
 var Some = _require$option.Some;
-var None = _require$option.None;
+var ZipkinNone = _require$option.None;
 var TraceId = _require.TraceId;
-
+var traceConfig = config.traceConfig; // how are we getting the config
 var url = require('url');
 
 function containsRequiredHeaders(req) {
-  return req.header(Header.TraceId) !== undefined && req.header(Header.SpanId) !== undefined;
+  return ((req.header(Header.TraceId) !== undefined && req.header(Header.SpanId) !== undefined) || req.header('traceId') !== undefined && req.header('spanId') !== undefined);
 }
 
 function stringToBoolean(str) {
@@ -45,68 +45,91 @@ module.exports = function expressMiddleware(_ref) {
   var port = _ref$port === undefined ? 0 : _ref$port;
 
   return function zipkinExpressMiddleware(req, res, next) {
-    tracer.scoped(function () {
-      function readHeader(header) {
-        var val = req.header(header);
-        if (val != null) {
-          return new Some(val);
-        } else {
-          return None;
-        }
-      }
-
-      if (containsRequiredHeaders(req)) {
-        var spanId = readHeader(Header.SpanId);
-        spanId.ifPresent(function (sid) {
-          var traceId = readHeader(Header.TraceId);
-          var parentSpanId = readHeader(Header.ParentSpanId);
-          var sampled = readHeader(Header.Sampled);
-          var flags = readHeader(Header.Flags).flatMap(stringToIntOption).getOrElse(0);
-          var id = new TraceId({
-            traceId: traceId,
-            parentId: parentSpanId,
-            spanId: sid,
-            sampled: sampled.map(stringToBoolean),
-            flags: flags
-          });
-          tracer.setId(id);
-        });
+      var newId = null;
+      var strTraceId = req.header('traceId');
+      var strSpanId  = req.header('spanId');
+      var rootFlag  = req.header('rootFlag');
+      if (rootFlag) {
+        serviceName = traceConfig.resourceResolverName;
       } else {
-        tracer.setId(tracer.createRootId());
-        if (req.header(Header.Flags)) {
-          var currentId = tracer.id;
-          var idWithFlags = new TraceId({
-            traceId: currentId.traceId,
-            parentId: currentId.parentId,
-            spanId: currentId.spanId,
-            sampled: currentId.sampled,
-            flags: readHeader(Header.Flags)
-          });
-          tracer.setId(idWithFlags);
-        }
+        serviceName = traceConfig.applicationServerName;;
       }
-
+      var spanId = tracer.createRootId();
+      if (!(strTraceId && strSpanId)) {
+          newId = new TraceId({
+              traceId: new Some(spanId.spanId),
+              spanId  : spanId.spanId
+            });
+          strTraceId = newId.traceId;
+          strSpanId = newId.spanId;
+        req['headers'].traceId = strTraceId;
+        req['headers'].spanId = strSpanId;
+        //req['headers'].rootFlag = rootFlag;
+      }
+    
+      
       var id = tracer.id;
-
+     
+      
+      
+      if (strTraceId && strSpanId) {
+        if(strTraceId === strSpanId) {
+          newId = new TraceId({
+                traceId: new Some(strTraceId),
+                spanId  : strSpanId,
+                parentId : ZipkinNone,
+                debug: new Some(true)
+              });
+        } else {
+          newId = new TraceId({
+                traceId: new Some(strTraceId),
+                parentId:new Some(strTraceId),
+                spanId  : spanId.spanId,
+                debug: new Some(true)
+              });
+        }
+        
+      } else {
+        newId = new TraceId({
+              traceId: new Some(spanId.spanId),
+              spanId  : spanId.spanId,
+              debug: new Some(true)
+            });
+        
+      }
+      res.set('X-TraceId', newId.traceId);
+      var startTime = new Date();
+      newId.timestamp = startTime.getTime();
+      tracer.setId(newId);
       tracer.recordServiceName(serviceName);
       tracer.recordRpc(req.method);
       tracer.recordBinary('http.url', formatRequestUrl(req));
-      tracer.recordAnnotation(new Annotation.ServerRecv());
       tracer.recordAnnotation(new Annotation.LocalAddr({ port: port }));
+      var serverRecv = new Annotation.ServerRecv();
+      serverRecv.timestamp = newId.timestamp;
+      tracer.recordAnnotation(serverRecv);
 
-      if (id.flags !== 0 && id.flags != null) {
-        tracer.recordBinary(Header.Flags, id.flags.toString());
-      }
+      req.traceId = newId.traceId;
+      req.spanId =  newId.spanId;
+      req.parentId = newId.parentId;
 
-      res.on('finish', function () {
-        tracer.scoped(function () {
-          tracer.setId(id);
-          tracer.recordBinary('http.status_code', res.statusCode.toString());
-          tracer.recordAnnotation(new Annotation.ServerSend());
-        });
+      res.on('finish', function (data) {
+        var serverSend = new Annotation.ServerSend();
+        var endTime = new Date();
+        
+        newId.timestamp = endTime.getTime();
+        newId.duration = endTime.getTime() - startTime.getTime();
+        tracer.setId(newId);
+        tracer.recordServiceName(serviceName);
+        //tracer.recordMessage('responseTime', newId.duration);
+        tracer.recordBinary('http.status_code', res.statusCode.toString());
+        tracer.recordAnnotation(new Annotation.LocalAddr({ port: port }));
+        serverSend.timestamp = endTime.getTime();
+        serverSend.duration = endTime.getTime() - startTime.getTime();
+        tracer.recordAnnotation(serverSend);
       });
-
+      
       next();
-    });
+    
   };
 };
